4d58dfc0166e50390a91853d8cd26d82c33e0fc7
alkmb <aleixkmb@gmail.com>
2024-08-24 15:20:46 +0200

    usus

--- module_3/ex00/includes/ClapTrap.hpp
+++ module_3/ex00/includes/ClapTrap.hpp
@@ -19,7 +19,7 @@ class ClapTrap
         ~ClapTrap();
         void attack(const std::string& target);
         void takeDamage(unsigned int amount);
-        void beRepaired(unsigned int amount);
+        void beRepaired(unsigned int amount); 
 
 };
 
