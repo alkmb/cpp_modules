6ffd3dbf6d4ae4128ef5ee643a38e1baaddb7f32
alkmb <contact@aleixkambou.dev>
2024-11-18 02:38:24 +0100

    module 7 should be completed but i will check wheni need to evaluate

---
+++ module_7/ex02/includes/Array.hpp
@@ -0,0 +1,70 @@
+#include <iostream>
+
+template <typename T> 
+
+class Array
+{
+  private:
+    
+    T* _arr;
+    size_t _size;
+
+  public:
+    
+    Array() : _arr( new T() ), _size(0) {};
+    Array(unsigned int n) : _arr( new T[n] ), _size(n) {};
+    
+    Array(const Array &src) : _arr( new T[src._size] ), _size(src._size) 
+    {
+      for (unsigned int i( 0 ); i < _size; i++ )
+        _arr[i] = src._arr[i];
+    };
+
+    Array &operator=(const Array &rhs)
+    {
+      if (this != &rhs)
+      {
+        delete []_arr;
+        _arr = new T[rhs.size()];
+        _size = rhs._size;
+
+        for (unsigned int i(0); i < _size; i++)
+          _arr[i] = rhs._arr[i];
+      }
+      return *this;
+    };
+
+    T& operator[](unsigned int i) const
+    {
+      if (i >= _size)
+        throw OutOfBoundsException();
+      return _arr[i];
+    };
+
+    class OutOfBoundsException: public std::exception
+    {
+      public:
+        virtual const char* what() const throw() 
+        {
+            return "Index is out of bounds.";
+        };
+    };
+
+    ~Array() 
+    {
+      delete []_arr;
+    };
+
+    unsigned int size(void) const
+    {
+      return _size;
+    };
+
+    friend std::ostream& operator<<( std::ostream& out, const Array<T>& arr)
+    {
+      for (unsigned int i(0); i < arr._size; i++)
+        out << arr[i] << " ";
+      return out;
+    };
+};
+
---
+++ module_7/ex02/src/main.cpp
@@ -0,0 +1,35 @@
+#include "../includes/Array.hpp"
+
+
+int main( void )
+{
+    Array< int > intArray( 9 );
+
+    for ( unsigned int i = 0; i < intArray.size(); i++ )
+        intArray[i] = i;
+    
+    std::cout << "Array1: " << intArray << std::endl;
+
+
+    try {
+        unsigned int sizeOfArray = 5;
+        Array<char> charArray(sizeOfArray);
+
+        // Set elements in the array
+        charArray[0] = 'A';
+        charArray[1] = 'B';
+        charArray[2] = 'C';
+        charArray[3] = 'D';
+        charArray[4] = 'E';
+
+        // Print the char array using overloaded operator<<
+        std::cout << "Char array: " << charArray << std::endl;
+
+        std::cout << "in bounds: " << intArray[5] << std::endl;
+        std::cout << "out of bounds: " << intArray[9] << std::endl;
+        std::cout << "out of bounds: " << intArray[19] << std::endl;
+    } catch ( Array< int >::OutOfBoundsException& e ) {
+        std::cout << "Error: " << e.what() << std::endl;
+    }
+    return ( 0 );
+}
