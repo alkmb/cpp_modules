228006062d76d0655843854a23f8d5efe852f782
alkmb <contact@aleixkambou.dev>
2024-12-15 09:30:27 +0100

    minor chnage

--- PART2/done/module_8/ex02/src/main.cpp
+++ PART2/done/module_8/ex02/src/main.cpp
@@ -21,5 +21,18 @@ std::cout << *it << std::endl;
 ++it;
 }
 std::stack<int> s(mstack);
+
+std::cout << "more test xd" << std::endl;
+
+mstack.push(48);
+mstack.push(14);
+mstack.push(45);
+mstack.push(24);
+
+while (it != mstack.end())
+{
+	std::cout << *it << std::endl;
+	it++;
+}
 return 0;
 }
