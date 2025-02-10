753222d51f1bf74aafb0349731bd788e9d1799cb
alkmb <contact@aleixkambou.dev>
2025-02-10 16:14:18 +0100

    removed .vscode and comments.

--- PART2/module_7/ex02/src/main.cpp
+++ PART2/module_7/ex02/src/main.cpp
@@ -11,24 +11,25 @@ int main( void )
     std::cout << "Array1: " << intArray << std::endl;
 
 
-    try {
+    try 
+    {
         unsigned int sizeOfArray = 5;
         Array<char> charArray(sizeOfArray);
 
-        // Set elements in the array
         charArray[0] = 'A';
         charArray[1] = 'B';
         charArray[2] = 'C';
         charArray[3] = 'D';
         charArray[4] = 'E';
 
-        // Print the char array using overloaded operator<<
         std::cout << "Char array: " << charArray << std::endl;
 
         std::cout << "in bounds: " << intArray[5] << std::endl;
         std::cout << "out of bounds: " << intArray[9] << std::endl;
         std::cout << "out of bounds: " << intArray[19] << std::endl;
-    } catch ( Array< int >::OutOfBoundsException& e ) {
+    }
+    catch ( Array< int >::OutOfBoundsException& e )
+    {
         std::cout << "Error: " << e.what() << std::endl;
     }
     return ( 0 );
